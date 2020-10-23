35882a38f42b66093413afb9edf586a0fe383a68
Yusent Chig <yusent@protonmail.com>
2020-10-23 14:18:27 -0700

    Add day2 part 2 solution

--- day2.js
+++ day2.js
@@ -1,18 +1,32 @@
 const fs = require('fs');
 
 fs.readFile('day2-input', 'utf8', (error, data) => {
-  const validPasswordsCount = data
-    .split('\n')
+  const lines = data.split('\n');
+  const validPasswordsCount1 = lines
     .reduce((count, line) => {
-      return line && checkPasswordLine(line) ? count + 1 : count;
+      return line && checkPasswordLine1(line) ? count + 1 : count;
+    }, 0);
+  const validPasswordsCount2 = lines
+    .reduce((count, line) => {
+      return line && checkPasswordLine2(line) ? count + 1 : count;
     }, 0);
 
-  console.log(validPasswordsCount);
+  console.log('Part 1:', validPasswordsCount1);
+  console.log('Part 2:', validPasswordsCount2);
 });
 
-const checkPasswordLine = line => {
+const checkPasswordLine1 = line => {
   const [_, min, max, char, password] = /^(\d+)-(\d+) (\w): (\w+)$/.exec(line);
   const occurrences = (password.match(new RegExp(char, 'g')) || []).length;
 
   return occurrences >= min && occurrences <= max;
+};
+
+const checkPasswordLine2 = line => {
+  const [_, pos0, pos1, char, password] = /^(\d+)-(\d+) (\w): (\w+)$/.exec(line);
+
+  if (password[pos0 - 1] === char)
+    return password[pos1 - 1] !== char;
+  else
+    return password[pos1 - 1] === char;
 };
