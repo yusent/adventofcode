c2052eb773f83bb9391b20f3e225086ef7455da0
Yusent Chig <yusent@protonmail.com>
2020-12-02 11:22:50 -0800

    Add day1 solutions in haskell

---
+++ 2020/day1.hs
@@ -0,0 +1,23 @@
+import Data.Set (Set, empty, insert, member)
+import qualified Data.Map.Strict as M (Map, (!), empty, insert, member)
+
+main :: IO ()
+main = do
+  expenses <- map read . lines <$> readFile "day1-input"
+  print $ findPairMultiplication empty expenses
+  print $ findTripleMultiplication M.empty [] expenses
+
+findPairMultiplication :: Set Int -> [Int] -> Int
+findPairMultiplication prev (x : xs)
+  | (2020 - x) `member` prev = x * (2020 - x)
+  | otherwise = findPairMultiplication (insert x prev) xs
+
+findTripleMultiplication :: M.Map Int Int -> [Int] -> [Int] -> Int
+findTripleMultiplication sums prev (x : xs)
+  | diff `M.member` sums = x * (sums M.! diff)
+  | otherwise = findTripleMultiplication updatedSums (x : prev) xs
+  where
+    diff = 2020 - x
+    updatedSums = updateSums prev sums
+    updateSums (p : ps) = updateSums ps . M.insert (x + p) (x * p)
+    updateSums _ = id
