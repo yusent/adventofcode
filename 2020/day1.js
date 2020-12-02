f14e92963c9a782ff8890435e41a3977eac4ebf7
Yusent Chig <yusent@protonmail.com>
2020-12-02 08:53:49 -0800

    Add day1 solutions

---
+++ 2020/day1.js
@@ -0,0 +1,26 @@
+fs = require('fs');
+
+fs.readFile('day1-input', 'utf8', (_, data) => {
+  expenses = new Set(data.split('\n').map(Number));
+  console.log('Part 1', findPairMultiplication(2020, expenses));
+  console.log('Part 2', findTripleMultiplication(expenses));
+});
+
+const findPairMultiplication = (sum, values) => {
+  for (const value of values) {
+    const complement = sum - value;
+
+    if (values.has(complement))
+      return value * complement;
+  }
+};
+
+const findTripleMultiplication = values => {
+  for (const value of values) {
+    const complement = 2020 - value;
+    const pairMult = findPairMultiplication(complement, values);
+
+    if (pairMult)
+      return value * pairMult;
+  }
+};
