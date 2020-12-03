0f786381814db22b590810a7c426d34cd07a52ed
Yusent Chig <yusent@protonmail.com>
2020-12-03 14:45:28 -0800

    Add day3 elixir solution

---
+++ 2020/day3.ex
@@ -0,0 +1,26 @@
+defmodule M do
+  def counter(rows = [row | _]) do
+    &count(rows, String.length(row), 0, 0, &1, &2)
+  end
+
+  defp count(rows, width, col, trees, right, down) do
+    case Enum.drop(rows, down) do
+      remaining_rows = [row | _] ->
+        new_col = rem(col + right, width)
+        char = String.at(row, new_col)
+        new_trees = trees + if(char == "#", do: 1, else: 0)
+        count(remaining_rows, width, new_col, new_trees, right, down)
+
+      _ ->
+        trees
+    end
+  end
+end
+
+{:ok, input} = File.read("day3-input")
+count = input |> String.trim() |> String.split("\n") |> M.counter()
+part1 = count.(3, 1)
+part2 = part1 * count.(1, 1) * count.(5, 1) * count.(7, 1) * count.(1, 2)
+
+IO.inspect(part1, label: "Part 1")
+IO.inspect(part2, label: "Part 2")
