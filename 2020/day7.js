ff4f71c6eef74887aebd691dff9810002f0ae4a1
Yusent Chig <yusent@protonmail.com>
2020-12-07 12:23:11 -0800

    Add day7 solution in JS

---
+++ 2020/day7.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+fs.readFile('input/day7', 'utf8', (_, data) => {
+  const colors = data.trim().split('\n').reduce((acc, line) => ({
+    ...acc,
+    ...parseLine(line)
+  }));
+
+  const countBagsIn = (color, factor = 1) => Object
+    .entries(colors[color])
+    .reduce((count, [subColor, qty]) => (
+      count + factor * qty + countBagsIn(subColor, factor * qty)
+    ), 0);
+
+  console.log('Part 1:', filterBags(colors).length);
+  console.log('Part 2:', countBagsIn('shiny gold'));
+});
+
+const parseLine = line => {
+  const [_, color, contents] = line.match(/^(\S+ \S+) bags contain (.*)\.$/);
+
+  return {
+    [color]: contents.split(', ').reduce((acc, content) => {
+      const match = content.match(/^(\d+) (\S+ \S+) bags?$/);
+
+      if (match) {
+        const [_, qty, subColor] = match;
+        return { ...acc, [subColor]: qty };
+      } else {
+        return acc;
+      }
+    }, {}),
+  };
+};
+
+const filterBags = (colors, bagsAcc = []) => {
+  const [newBagsAcc, keepGoing] = Object
+    .entries(colors)
+    .reduce(colorsReducer, [bagsAcc, false]);
+
+  return keepGoing ? filterBags(colors, newBagsAcc) : newBagsAcc;
+};
+
+const colorsReducer = ([bagsAcc, keepGoing], [color, subColors]) => Object
+  .keys(subColors)
+  .reduce(subColorsReducer(color, bagsAcc), [bagsAcc, keepGoing]);
+
+const subColorsReducer = (color, bagsAcc) => ([acc, keep], k) => (
+  (k === 'shiny gold' || bagsAcc.includes(k)) && !acc.includes(color)
+    ? [[...acc, color], true]
+    : [acc, keep]
+);
