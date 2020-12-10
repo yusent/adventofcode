f3f0c9a15e79a3fe7ffa03f93817da19e25389aa
Yusent Chig <yusent@protonmail.com>
2020-12-09 21:00:48 -0800

    Refactor day9 haskell solution

--- 2020/day9.hs
+++ 2020/day9.hs
@@ -1,12 +1,12 @@
 import Data.Maybe (fromJust, isJust)
-import Data.List (tails, sort)
+import Data.List (tails)
 
 main :: IO ()
 main = do
   numbers <- map read . lines <$> readFile "input/day9"
   let invalidNumber = findInvalid numbers
-      weaknessSubList = sort $ findWeaknessSubList invalidNumber numbers
-      weakness = head weaknessSubList + last weaknessSubList
+      weaknessSubList = findWeaknessSubList invalidNumber numbers
+      weakness = minimum weaknessSubList + maximum weaknessSubList
   putStrLn $ "Part 1: " ++ show invalidNumber
   putStrLn $ "Part 2: " ++ show weakness
 
@@ -15,7 +15,7 @@ findInvalid numbers@(_ : next)
   | valid n prev = findInvalid next
   | otherwise = n
  where
-   (prev, (n : _)) = splitAt 25 numbers
+   (prev, n : _) = splitAt 25 numbers
 
 valid :: Int -> [Int] -> Bool
 valid n (x : xs) = n - x `elem` xs || valid n xs
