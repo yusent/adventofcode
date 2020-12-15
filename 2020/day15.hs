9a5385e31217f13e1e9008540900ad2846979ce3
Yusent Chig <yusent@protonmail.com>
2020-12-15 10:01:34 -0800

    Refactor day15 Haskell solution

--- 2020/day15.hs
+++ 2020/day15.hs
@@ -2,11 +2,11 @@ import Data.Map.Strict (Map, (!?), fromList, insert)
 
 main :: IO ()
 main = do
-  let input = fromList [(2, 1), (1, 2), (10, 3), (11, 4), (0, 5)]
-  putStrLn . ("Part 1: " ++) . show $ entries input 6 2020 6
-  putStrLn . ("Part 2: " ++) . show $ entries input 6 30000000 6
+  let input = [2, 1, 10, 11, 0, 6]
+      mem = fromList $ zip (init input) [1..]
+      numbers = 0 : (init input) ++ genNumList mem (length input) (last input)
+      genNumList m i n = seq n
+        $ n : genNumList (insert n i m) (i + 1) (maybe 0 (i -) $ m !? n)
 
-entries :: Map Int Int -> Int -> Int -> Int -> Int
-entries m i u n
-  | i == u = n
-  | otherwise = seq n . entries (insert n i m) (i + 1) u . maybe 0 (i -) $ m !? n
+  putStrLn . ("Part 1: " ++) . show $ numbers !! 2020
+  putStrLn . ("Part 2: " ++) . show $ numbers !! 30000000
