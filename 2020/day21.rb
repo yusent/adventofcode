447395b36f21ffb6a5c6e0996546a09a09b1e176
Yusent Chig <yusent@protonmail.com>
2020-12-20 22:15:21 -0800

    Add day21 solution in Ruby

---
+++ 2020/day21.rb
@@ -0,0 +1,35 @@
+require "set"
+
+allergens = {}
+ingredients = {}
+
+File.read("input/day21").chomp.split("\n").each do |line|
+  ingredients_str, allergens_str = line[0..-2].split(" (contains ")
+
+  ingredients_set = Set.new(ingredients_str.split.map do |ingredient|
+    ingredients[ingredient] = ingredients[ingredient].to_i + 1
+    ingredient
+  end)
+
+  allergens_str.split(', ').each do |allergen|
+    allergens[allergen] = allergens.has_key?(allergen) \
+      ? allergens[allergen] & ingredients_set \
+      : ingredients_set
+  end
+end
+
+loop do
+  allergen_pair = allergens.find { |_, v| v.is_a?(Set) && v.size == 1 }
+
+  break if allergen_pair.nil?
+
+  allergen = allergen_pair[0]
+  ingredient = allergen_pair[1].to_a[0]
+  allergens[allergen] = ingredient
+  ingredients.delete(ingredient)
+
+  allergens.transform_values! { |s| s.is_a?(Set) ? s.delete(ingredient) : s }
+end
+
+puts "Part 1: #{ingredients.values.sum}"
+puts "Part 2: #{allergens.keys.sort.map { |k| allergens[k] }.join(",")}"
