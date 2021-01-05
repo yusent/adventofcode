d9e8d8179c33479deb4945319edc5a47847ec70a
Yusent Chig <yusent@protonmail.com>
2021-01-05 10:26:29 -0800

    Adjust IntCode to return multiple outputs

--- 2019/IntCode.hs
+++ 2019/IntCode.hs
@@ -5,6 +5,7 @@ module IntCode
   , execUntilHalt
   , getOutput
   , load
+  , reverseOutput
   , run
   ) where
 
@@ -15,10 +16,10 @@ import Data.Map.Strict (Map, findWithDefault, fromList, insert)
 type Instructions = Map Int Int
 type Pointer = Int
 type Input = [Int]
-type Output = Maybe Int
+type Output = [Int]
 data State = Initial Instructions Input
            | WaitingForInput Instructions Pointer Int Input Output
-           | Halted Int
+           | Halted Output
 
 load :: String -> Instructions
 load = fromList . zip [0..] . map read . splitOn ","
@@ -27,15 +28,20 @@ addInput :: State -> Int -> State
 addInput (Initial ops input) x = Initial ops (input ++ [x])
 addInput (WaitingForInput ops index rBase input output) x =
   WaitingForInput ops index rBase (input ++ [x]) output
+addInput (Halted output) _ = Halted output
 
 getOutput :: State -> Output
 getOutput (WaitingForInput _ _ _ _ output) = output
-getOutput (Halted output) = Just output
+getOutput (Halted output) = output
+
+reverseOutput :: State -> State
+reverseOutput (WaitingForInput ins pnt idx input output) =
+  WaitingForInput ins pnt idx input $ reverse output
+reverseOutput (Halted output) = Halted $ reverse output
 
 run :: State -> State
-run (Initial ops input) = exec ops input Nothing 0 0
-run (WaitingForInput ops index rBase input@(_ : _) _) =
-  exec ops input Nothing rBase index
+run (Initial ops input) = exec ops input [] 0 0
+run (WaitingForInput ops index rBase input@(_ : _) _) = exec ops input [] rBase index
 run state = state
 
 exec :: Instructions -> Input -> Output -> Int -> Pointer -> State
@@ -45,13 +51,13 @@ exec ops input output rBase index = case opCode of
   3 -> if null input
           then WaitingForInput ops index rBase [] output
           else exec (storeAt 1 $ head input) (tail input) output rBase $ index + 2
-  4 -> exec ops input (Just $ op' 1) rBase $ index + 2
+  4 -> exec ops input (op' 1 : output) rBase $ index + 2
   5 -> exec ops input output rBase $ if op' 1 /= 0 then op' 2 else index + 3
   6 -> exec ops input output rBase $ if op' 1 == 0 then op' 2 else index + 3
   7 -> exec (storeAt 3 $ if op' 1 < op' 2 then 1 else 0) input output rBase $ index + 4
   8 -> exec (storeAt 3 $ if op' 1 == op' 2 then 1 else 0) input output rBase $ index + 4
   9 -> exec ops input output (rBase + op' 1) $ index + 2
-  99 -> Halted $ fromMaybe (findWithDefault 0 0 ops) output
+  99 -> Halted $ if null output then [findWithDefault 0 0 ops] else output
   unknown -> error $ "Unknown op code: " ++ show unknown
   where
     (mode, opCode) = op 0 `divMod` 100
@@ -70,5 +76,5 @@ exec ops input output rBase index = case opCode of
 
 execUntilHalt :: Instructions -> Input -> Int
 execUntilHalt ops input = case run (Initial ops input) of
-  Halted output -> output
+  Halted (output : _) -> output
   _ -> error "Not enough input"
