62e949e2e483313501a50d88f8bb61611789cb12
Yusent Chig <yusent@protonmail.com>
2021-12-01 15:58:13 -0800

    Add 2021-day01 solution in Elixir

---
+++ 2021/day01.ex
@@ -0,0 +1,27 @@
+defmodule Solution do
+  def count_increments(window_size, xs) do
+    [first_window | windows] = window_sums(window_size, xs)
+
+    windows
+    |> Enum.reduce({0, first_window}, fn x, {acc, prev} ->
+      {acc + if(x > prev, do: 1, else: 0), x}
+    end)
+    |> elem(0)
+  end
+
+  defp window_sums(size, xs) when length(xs) < size, do: []
+  defp window_sums(size, [_ | rest] = xs) do
+    sum = xs |> Enum.take(size) |> Enum.sum()
+    [sum | window_sums(size, rest)]
+  end
+end
+
+depths =
+  "input/day01"
+  |> File.read!()
+  |> String.trim()
+  |> String.split("\n")
+  |> Enum.map(&String.to_integer/1)
+
+IO.inspect(Solution.count_increments(1, depths), label: "Part 1")
+IO.inspect(Solution.count_increments(3, depths), label: "Part 2")
