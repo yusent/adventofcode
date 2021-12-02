f639d8cde0ab31d402bf18e97b6ba7343b845be7
Yusent Chig <yusent@protonmail.com>
2021-12-02 14:07:41 -0800

    Refactor 2021-day01 Elixir solution

--- 2021/day01.ex
+++ 2021/day01.ex
@@ -3,8 +3,8 @@ defmodule Day01 do
     depths =
       "input/day01"
       |> File.read!()
-      |> (&Regex.scan(~r/\S+/, &1)).()
-      |> Enum.map(&String.to_integer(hd(&1)))
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
 
     IO.inspect(count_increments(1, depths), label: "Part 1")
     IO.inspect(count_increments(3, depths), label: "Part 2")
@@ -14,9 +14,7 @@ defmodule Day01 do
     xs
     |> Enum.drop(window_size)
     |> Enum.zip(xs)
-    |> Enum.reduce(0, fn {current, prev}, count ->
-      count + if(current > prev, do: 1, else: 0)
-    end)
+    |> Enum.count(fn {current, prev} -> current > prev end)
   end
 end
 
