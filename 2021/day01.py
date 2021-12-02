1e6146584ac4e0b46020662010e6002341974571
Yusent Chig <yusent@protonmail.com>
2021-12-02 14:39:56 -0800

    Refactor 2021-day01 Python solution

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,5 +1,5 @@
 def count_increments(w, xs):
-    return sum(1 if xs[i] > xs[i-w] else 0 for i in range(w, len(xs)))
+    return sum(int(xs[i] > xs[i-w]) for i in range(w, len(xs)))
 
 if __name__ == "__main__":
     depths = list(map(int, open("input/day01", "r").read().split()))
