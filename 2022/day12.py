5ff070367d3daf28f580a0b1034b00aef4ea838d
Yusent Chig <yusent@protonmail.com>
2022-12-12 13:51:31 -0800

    Refactor day12 solution

--- 2022/day12.py
+++ 2022/day12.py
@@ -12,17 +12,17 @@ def min_steps(part2=False):
     for i, row in enumerate(grid):
         for j, cell in enumerate(row):
             if cell == 'S' or part2 and cell == 'a':
-                q.append(((i, j), 0))
+                q.append((i, j, 0))
 
     while q:
-        (i, j), steps = q.popleft()
+        i, j, steps = q.popleft()
         if (i, j) in visited: continue
         if grid[i][j] == 'E': return steps
         visited.add((i, j))
         for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
             y, x = i + di, j + dj
             if 0 <= y < len(grid) and 0 <= x < len(grid[0]) and heights[y][x] <= 1 + heights[i][j]:
-                q.append(((y, x), steps + 1))
+                q.append((y, x, steps + 1))
 
 grid = open('input/day12').read().splitlines()
 heights = [[char_value(c) for c in row] for row in grid]
