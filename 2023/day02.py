3e5f14f214fc1dd4dcaed4c41ed4d1708e6149f2
Yusent Chig <yusent@protonmail.com>
2023-12-02 12:27:48 -0800

    Refactor day02 solution: Capture number instead of splitting

--- 2023/day02.py
+++ 2023/day02.py
@@ -4,9 +4,9 @@ possigle_game_ids_sum = 0
 set_powers_sum = 0
 
 for game in open('input/day02', 'r').read().strip().split('\n'):
-    max_red = max(int(m.split(' ')[0]) for m in re.findall(r'\d+ red', game))
-    max_green = max(int(m.split(' ')[0]) for m in re.findall(r'\d+ green', game))
-    max_blue = max(int(m.split(' ')[0]) for m in re.findall(r'\d+ blue', game))
+    max_red = max(map(int, re.findall(r'(\d+) red', game)))
+    max_green = max(map(int, re.findall(r'(\d+) green', game)))
+    max_blue = max(map(int, re.findall(r'(\d+) blue', game)))
     set_powers_sum += max_red * max_green * max_blue
 
     if max_red <= 12 and max_green <= 13 and max_blue <= 14:
